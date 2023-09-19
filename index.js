3dd09084f590897b1e37dd9b22d4ceae4838e1b0
mlucchelli <mario_no_soul@hotmail.com>
2023-09-19 19:18:36 -0300

    cma-6: add support to currencies and crypto

--- index.js
+++ index.js
@@ -24,20 +24,67 @@ app.get('/latest', async (request, response) => {
       "blue": "dolar_blue"
     };
 
-    const dolarEuroResponse = {};
+    const dolarData= {};
 
     for (const apiProperty in propertyMapping1) {
       if (data1.hasOwnProperty(apiProperty)) {
-        dolarEuroResponse[propertyMapping1[apiProperty]] = {
+        dolarData[propertyMapping1[apiProperty]] = {
           "value_avg": data1[apiProperty].value_avg.toFixed(2),
           "value_sell": data1[apiProperty].value_sell.toFixed(2),
           "value_buy": data1[apiProperty].value_buy.toFixed(2),
         };
       }
     }
+
+    const currencies = ['EUR', 'GBP' ,'JPY']
+    const internationalCurrenciesData = {};
+    try {
+        for (const currency of currencies) {
+
+          const response = await axios.get(`https://api.exchangerate.host/convert?from=USD&to=${currency}`);
+          const data = response.data;
     
+          if (data.success) {
+            internationalCurrenciesData[currency] = {
+                "value_avg": data.result.toFixed(2),
+                //"value_sell": data1[apiProperty].value_sell.toFixed(2),
+                //"value_buy": data1[apiProperty].value_buy.toFixed(2),
+              };
+          } else {
+            console.error(`Failed to fetch data for ${currency}`);
+          }
+        }
+      }
+      catch (error) {
+        console.error(`Error fetching data for ${currency}: ${error.message}`);
+      }
+
+      const cryptos = ['BTC', 'ETH']
+      const CryptoData = {};
+      try {
+          for (const crypto of cryptos) {
+            const response = await axios.get(`https://api.exchangerate.host/convert?from=${crypto}&to=USD`);
+            const data = response.data;
+
+            if (data.success) {
+                CryptoData[crypto] = {
+                  "value_avg": data.result.toFixed(2),
+                  //"value_sell": data1[apiProperty].value_sell.toFixed(2),
+                  //"value_buy": data1[apiProperty].value_buy.toFixed(2),
+                };
+            } else {
+              console.error(`Failed to fetch data for ${crypto}`);
+            }
+          }
+        }
+        catch (error) {
+          console.error(`Error fetching data for ${crypto}: ${error.message}`);
+        }
+
     currency_data = {
-      ...dolarEuroResponse,
+      ...dolarData,
+      ...internationalCurrenciesData,
+      ...CryptoData
     };
 
     console.log(JSON.stringify(currency_data, null, 2));
